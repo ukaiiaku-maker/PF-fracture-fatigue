a7f0923d344aec270bf6856846acbb6bd1e3ef36
ukaiiaku-maker <ukaiiaku@gmail.com>
2026-07-16 11:59:52 -0700

    Expose v10.0.2.1 package version

--- arrhenius_fracture/__init__.py
+++ arrhenius_fracture/__init__.py
@@ -1,6 +1,6 @@
 """Arrhenius sharp-front fracture and fatigue with unified MPZ state."""
 
-__version__ = "10.0.2"
+__version__ = "10.0.2.1"
 
 from .material_manifest import MaterialManifest
 from .unified_mpz import MPZConfig, UnifiedMPZState
