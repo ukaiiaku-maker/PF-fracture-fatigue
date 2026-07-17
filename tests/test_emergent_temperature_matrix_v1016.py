ff10feafd49eac0e91ea55b42aae7062d23f28ac
ukaiiaku-maker <ukaiiaku@gmail.com>
2026-07-16 18:49:34 -0700

    Update matrix version assertion for 10.1.6.1

--- tests/test_emergent_temperature_matrix_v1016.py
+++ tests/test_emergent_temperature_matrix_v1016.py
@@ -19,7 +19,7 @@ def _analyzer_module():
 
 
 def test_version_is_v1016():
-    assert arrhenius_fracture.__version__ == "10.1.6"
+    assert arrhenius_fracture.__version__ == "10.1.6.1"
 
 
 def test_entry_point_changes_no_constitutive_temperature_law():
