2d568fb785b2e284cfbbef1e7d1f06e011f0c178
ukaiiaku-maker <ukaiiaku@gmail.com>
2026-07-18 09:16:26 -0700

    Test the actual fixed-DeltaK waveform namespace

--- tests/test_fixed_deltaK_v1021.py
+++ tests/test_fixed_deltaK_v1021.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from arrhenius_fracture import fatigue_v1, sharp_front
+from arrhenius_fracture import fatigue_v1
 from arrhenius_fracture.fixed_deltaK_v1021 import (
     FixedDeltaKConfig,
     fixed_deltaK_audit_payload,
@@ -29,17 +29,15 @@ def test_factory_replaces_incoming_kmax_exactly():
     assert np.isclose(audit["incoming_Kmax_min_Pa_sqrt_m"], 99.0e6)
 
 
-def test_context_patches_driver_and_restores_symbols():
-    original_driver = sharp_front.FatigueWaveform
+def test_context_patches_defining_module_and_restores_symbol():
     original_module = fatigue_v1.FatigueWaveform
     with install_fixed_deltaK_waveform(24.0):
-        assert sharp_front.FatigueWaveform is not original_driver
-        wave = sharp_front.FatigueWaveform(
+        assert fatigue_v1.FatigueWaveform is not original_module
+        wave = fatigue_v1.FatigueWaveform(
             Kmax=1.0e6, R=0.2, frequency_Hz=500.0, closure_clip=True
         )
         assert np.isclose(wave.DeltaK, 24.0e6)
         assert np.isclose(wave.Kmax, 30.0e6)
-    assert sharp_front.FatigueWaveform is original_driver
     assert fatigue_v1.FatigueWaveform is original_module
 
 
