de154c8b29a0ee290feb8f00763fae94c39a5cad
ukaiiaku-maker <ukaiiaku@gmail.com>
2026-07-19 10:34:58 -0700

    Add active loader schema smoke test

---
+++ tests/test_v10214_active_loader_model_id.py
@@ -0,0 +1,5 @@
+from arrhenius_fracture.signed_kernel_family_v10214 import SCHEMA
+
+
+def test_v10214_active_loader_schema():
+    assert SCHEMA == "v10.2.14_active_only_real_signed_2d_shielding_atlas"
