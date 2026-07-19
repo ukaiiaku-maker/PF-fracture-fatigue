f44039dca00db6e65c9678708119c73056bdaa8f
ukaiiaku-maker <ukaiiaku@gmail.com>
2026-07-19 10:33:59 -0700

    Add load-invariance import smoke test

---
+++ tests/test_v10214_preflight_import.py
@@ -0,0 +1,5 @@
+from arrhenius_fracture.frozen_geometry_load_invariance_v10213 import MODEL_ID
+
+
+def test_v10214_load_invariance_model_id():
+    assert MODEL_ID == "v10.2.14_active_frozen_geometry_load_invariance"
